c19cd1a2d5166f447f626160832ca95425a24604
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-12-21 15:40:40 +0300

    feat: implement `Flight` model

---
+++ src/main/java/com/amadeus/api/flight/Flight.java
@@ -0,0 +1,74 @@
+package com.amadeus.api.flight;
+
+import com.amadeus.api.airport.Airport;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Entity
+@Table(name = "Flights")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Flight {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime departureTime;
+
+    @Column(nullable = false)
+    private LocalDateTime arrivalTime;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal ticketPrice;
+
+    @Column(nullable = false, length = 3)
+    private String ticketCurrency;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departure_airport_id", nullable = false, unique = true)
+    private Airport departureAirport;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "arrival_airport_id", nullable = false, unique = true)
+    private Airport arrivalAirport;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private OffsetDateTime lastUpdated;
+
+}
