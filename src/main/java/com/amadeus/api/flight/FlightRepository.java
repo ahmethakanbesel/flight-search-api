58225b82cb290ee4614bfe390a49c491215b1dbb
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-12-21 21:17:28 +0300

    feat: add `FlightRepository` functions for searching flights

--- src/main/java/com/amadeus/api/flight/FlightRepository.java
+++ src/main/java/com/amadeus/api/flight/FlightRepository.java
@@ -2,6 +2,19 @@ package com.amadeus.api.flight;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByDepartureAirportCityAndArrivalAirportCityAndDepartureTimeBetween(
+            String departureCity, String arrivalCity,
+            LocalDateTime departureTimeStart, LocalDateTime departureTimeEnd);
+
+    List<Flight> findByDepartureAirportCityAndArrivalAirportCityAndDepartureTimeBetweenAndReturnTimeBetween(
+            String departureCity, String arrivalCity,
+            LocalDateTime departureTimeStart, LocalDateTime departureTimeEnd,
+            LocalDateTime returnTimeStart, LocalDateTime returnTimeEnd);
+
+    List<Flight> findByDepartureAirportIdOrArrivalAirportId(Long airport, Long airport1);
 }
