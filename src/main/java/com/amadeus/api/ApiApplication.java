d7446c327b48e1da698361e36e5a0a2ce703be14
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-12-21 15:54:45 +0300

    feat: add security scheme info

--- src/main/java/com/amadeus/api/ApiApplication.java
+++ src/main/java/com/amadeus/api/ApiApplication.java
@@ -1,10 +1,17 @@
 package com.amadeus.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Flights API", version = "1.0", description = "Management service for flights"))
+@SecurityScheme(name = "InMemory", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class ApiApplication {
 
     public static void main(final String[] args) {
