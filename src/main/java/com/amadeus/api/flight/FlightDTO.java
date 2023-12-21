7235402ffffc3770ba47bd5062ac2745efc26b75
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-12-21 21:16:44 +0300

    feat: add schema descriptions to `FlightDTO`

--- src/main/java/com/amadeus/api/flight/FlightDTO.java
+++ src/main/java/com/amadeus/api/flight/FlightDTO.java
@@ -18,25 +18,31 @@ public class FlightDTO {
     private Long id;
 
     @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
+    @Schema(type = "string", example = "2024-01-10 09:00")
     private LocalDateTime departureTime;
 
-    @NotNull
-    private LocalDateTime arrivalTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
+    @Schema(type = "string", example = "2024-01-14 19:30")
+    private LocalDateTime returnTime;
 
     @NotNull
     @Digits(integer = 10, fraction = 2)
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    @Schema(type = "string", example = "49.08")
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT)
+    @Schema(type = "number", example = "49.08")
     private BigDecimal ticketPrice;
 
     @NotNull
     @Size(max = 3)
+    @Schema(type = "string", example = "USD")
     private String ticketCurrency;
 
     @NotNull
+    @Schema(type = "number", example = "10000")
     private Long departureAirport;
 
     @NotNull
+    @Schema(type = "number", example = "10002")
     private Long arrivalAirport;
 
 }
