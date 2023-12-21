737bc7d6829457b84800551a7c66fb519dc33676
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-12-21 21:16:54 +0300

    feat: add schema descriptions to `AirportDTO`

--- src/main/java/com/amadeus/api/airport/AirportDTO.java
+++ src/main/java/com/amadeus/api/airport/AirportDTO.java
@@ -1,5 +1,6 @@
 package com.amadeus.api.airport;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -10,10 +11,12 @@ import lombok.Setter;
 @Setter
 public class AirportDTO {
 
+    @Schema(type = "number", example = "")
     private Long id;
 
     @NotNull
     @Size(max = 255)
+    @Schema(type = "string", example = "Istanbul")
     private String city;
 
 }
